518d2dad441a8860657be191a1a6694f04be44ce
Kimbangul <highcolor_12@naver.com>
2023-05-23 16:31:47 +0900

    feat: riot api add

--- src/app/api/account/route.ts
+++ src/app/api/account/route.ts
@@ -1,11 +1,15 @@
-import client from "@/app/axios/client";
+import {apiClient} from "@/app/axios/client";
+import URL from "@/app/api/url.json";
 import { NextResponse } from "next/server";
 
-
 export async function GET(request: Request){
-  const dummy = {'sample': 'sample'};
+  const name='헤비메탈 아이돌';
+  const API_URL = `${URL["SUMMONER-V4"]}/${name}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`;
 
-  const res = JSON.stringify(dummy);
+  const data = await apiClient.get(API_URL);
+  // const dummy = {'sample': 'sample'};
+
+  const res = JSON.stringify(data);
 
   return NextResponse.json({res});
 }
