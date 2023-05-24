90cfc66e3321cf17f2a24d80e4efc65335c97b2f
Kimbangul <highcolor_12@naver.com>
2023-05-24 15:48:01 +0900

    feat: change useQuery to useQueries

--- src/components/home/HomeContainer.tsx
+++ src/components/home/HomeContainer.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useMemo, useCallback } from "react";
 import { AxiosResponse } from "axios";
-import { useQuery } from "react-query";
+import { useQuery, useQueries } from "react-query";
 
 import {ResponseType} from '@/app/api/account/type';
 import client from "@/app/axios/client";
@@ -16,27 +16,51 @@ const HomeContainer = () => {
   const [inputName, setInputName] = useState('');
 
   // FUNCTION data fetch
-  const { isFetching, data, error, refetch } = useQuery(
-    'summonerInfo',
-    async () => {
-      const res = await client.get(`/api/account`, {params: {name: inputName}});
-      console.log(res);
-      return res.data;
+  // const { isFetching, data, error, refetch } = useQuery(
+  //   'summonerInfo',
+  //   async () => {
+  //     const res = await client.get(`/api/account`, {params: {name: inputName}});
+  //     console.log(res);
+  //     return res.data;
+  //   },
+  //   {
+  //     enabled: false,
+  //   }
+  // );
+  const result = useQueries([
+    {
+      queryKey: 'summonerInfo', 
+      queryFn: async () => {
+          const res = await client.get(`/api/account`, {params: {name: inputName}});
+          console.log(res);
+          return res.data;
+        },
+      refetchOnMount: false,
+      enabled: false,
     },
     {
+      queryKey: 'matchInfo',
+      queryFn: async() => {
+          const res = await client.get(`/api/account`, {params: {name: inputName}});
+          console.log(res);
+          return res.data;
+      },
+      refetchOnMount: false,
       enabled: false,
     }
-  );
+  ]);
+
 
   // FUNCTION onclick events
   const getSummonerInfo = () => {
-    refetch();
+    // refetch();
+    result[0].refetch();
   }
 
   const getResultView = useMemo(()=>{
-    switch(data?.status){
+    switch(result[0].data?.status){
       case 200:
-        return <ResultView data={data?.data}/>
+        return <ResultView data={result[0].data.data}/>
       case 404:
         return (
           <p>소환사 정보가 없습니다.</p>
@@ -44,18 +68,18 @@ const HomeContainer = () => {
       default:
         return null;
     }
-  }, [data]);
+  }, [result[0].data]);
 
 
   useEffect(()=>{
-    console.log(isFetching)
-  }, [isFetching]);
+    console.log(result[0].data)
+  }, [result[0].data]);
 
   return(
    <>
     <InputView inputName={inputName} setInputName={setInputName} getSummonerInfo={getSummonerInfo}/>
     {
-      isFetching 
+      result[0].isFetching 
       ?
       <>Loading</>
       :
