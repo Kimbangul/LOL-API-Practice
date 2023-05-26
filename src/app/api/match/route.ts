1e410c0dd8bf09c1a61faeb5b7dc79ebc807ce14
Kimbangul <highcolor_12@naver.com>
2023-05-26 10:23:49 +0900

    chore: install moment

--- src/app/api/match/route.ts
+++ src/app/api/match/route.ts
@@ -6,7 +6,7 @@ export async function GET(req: Request, res: Response){
   const reqUrl = new URL(req.url);
   const puuid = reqUrl.searchParams.get('puuid');
 
-  const API_URL = `${APIURL["MATCH-V5"]}/${puuid}/ids?start=0&count=20&api_key=${process.env.NEXT_PUBLIC_API_KEY}`;
+  const API_URL = `${APIURL["MATCH-V5"]}/${puuid}/ids?start=0&count=50&api_key=${process.env.NEXT_PUBLIC_API_KEY}`;
 
   const data = await asiaApiClient.get(API_URL);
 
