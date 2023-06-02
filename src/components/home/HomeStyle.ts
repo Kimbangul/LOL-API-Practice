f2a7dc62cbe27cd1b565200f04a3770eb107a99a
Kimbangul <highcolor_12@naver.com>
2023-06-02 16:32:07 +0900

    style: input styling

--- src/components/home/HomeStyle.ts
+++ src/components/home/HomeStyle.ts
@@ -11,12 +11,17 @@ const Home = {
   `,
   Container: styled.div`
     max-width: 1280px;
+    width: 100%;
     max-height: 640px;
     overflow: auto;
     padding: 2.4rem;
     border-radius: 1.6rem;
     backdrop-filter: blur(2rem);
-    background-color: rgba(16,18,27, 0.4);
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0 25px 45px rgba(0, 0, 0, 0.1);
+    border: 1px solid rgba(255, 255, 255, 0.5);
+    border-right: 1px solid rgba(255, 255, 255, 0.2);
+    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
   `,
 }
 
--- src/components/home/InputStyle.ts
+++ src/components/home/InputStyle.ts
@@ -3,23 +3,55 @@ import styled from "styled-components";
 const Input = {
   Container: styled.form`
     display: flex;
+    height: 4.9rem;
+    justify-content: center;
     align-items: center;
+    gap: 0 1.6rem;
   `,
   Input: styled.input`
     outline: none;
-    border: 0.1rem solid #ddd;
-    padding: 0.8rem 1.6rem;
-    border-radius: 0.4rem;
-    background: rgb(20, 22, 43);
-    color: rgb(249, 250, 251);
+    width: 50%;
+    padding: 1.6rem 2.4rem;
+    letter-spacing: 0.1rem;
+    border-radius: 0.8rem;
+    background: rgba(255, 255, 255, 0.2);
+    color: #fff;
+    border: 0.1rem solid rgba(255, 255, 255, 0.5);
+    box-shadow: 0 0.5rem 1.5rem rgba(0, 0, 0, 0.05);
+    border-right: 0.1rem solid rgba(255, 255, 255, 0.2);
+    border-bottom: 0.1rem solid rgba(255, 255, 255, 0.2);
+    transition: background 0.3s, color 0.3s;
+
+    &:focus{
+      background: rgba(255, 255, 255, 0.6);
+      color: #333;
+    }
 
     &::placeholder{
-      color: rgba(113, 119, 144, 0.78);
+      color: rgba(255,255,255, 0.5);
     }
   `,
   Button: styled.button`
+    width: 6rem;
+    height: 100%;
+    display: inline-block;
+    align-self: stretch;
     padding: 0.4rem 0.8rem;
+    color: #666;
     cursor: pointer;
+    background: #fff;
+    max-width: 10rem;
+    font-weight: 600;
+    border: 0.1rem solid rgba(255, 255, 255, 0.5);
+    box-shadow: 0 0.5rem 1.5rem rgba(0, 0, 0, 0.05);
+    border-right: 0.1rem solid rgba(255, 255, 255, 0.2);
+    border-bottom: 0.1rem solid rgba(255, 255, 255, 0.2);
+    border-radius: 0.8rem;
+    transition: transform 0.3s;
+
+    &:active{
+      transform: scale(0.9);
+    }
   `
 }
 
